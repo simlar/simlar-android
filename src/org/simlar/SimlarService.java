622fd8f54fe82f93304a2ddb063efaa95b9c024d
Ben Sartor <mail@ben-sartor.de>
2013-11-18 06:30:22 +0100

    [SimlarService] triggers refreshRegisters on network change

--- src/org/simlar/SimlarService.java
+++ src/org/simlar/SimlarService.java
@@ -33,11 +33,15 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.BroadcastReceiver;
 import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.database.Cursor;
 import android.graphics.BitmapFactory;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.net.wifi.WifiManager;
 import android.net.wifi.WifiManager.WifiLock;
@@ -72,12 +76,22 @@ public class SimlarService extends Service implements LinphoneHandlerListener
 	private Class<?> mNotificationActivity = null;
 	private VibratorThread mVibratorThread = null;
 	private RingtoneThread mRingtoneThread = null;
+	private NetworkChangeReceiver mNetworkChangeReceiver = new NetworkChangeReceiver();
 
 	public class SimlarServiceBinder extends Binder
 	{
 		SimlarService getService()
 		{
 			return SimlarService.this;
+		}
+	}
+
+	class NetworkChangeReceiver extends BroadcastReceiver
+	{
+		@Override
+		public void onReceive(Context context, Intent intent)
+		{
+			SimlarService.this.checkNetworkConnectivityAndRefreshRegisters();
 		}
 	}
 
@@ -165,6 +179,10 @@ public class SimlarService extends Service implements LinphoneHandlerListener
 
 		mLinphoneThread = new LinphoneThread(this, this);
 
+		IntentFilter intentFilter = new IntentFilter();
+		intentFilter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
+		registerReceiver(mNetworkChangeReceiver, intentFilter);
+
 		mHandler.post(new Runnable() {
 			@Override
 			public void run()
@@ -251,6 +269,8 @@ public class SimlarService extends Service implements LinphoneHandlerListener
 	{
 		Log.i(LOGTAG, "onDestroy");
 
+		unregisterReceiver(mNetworkChangeReceiver);
+
 		// just in case
 		if (mWakeLock.isHeld()) {
 			mWakeLock.release();
@@ -264,6 +284,21 @@ public class SimlarService extends Service implements LinphoneHandlerListener
 		Toast.makeText(this, R.string.simlarservice_on_destroy, Toast.LENGTH_SHORT).show();
 
 		Log.i(LOGTAG, "onDestroy ended");
+	}
+
+	void checkNetworkConnectivityAndRefreshRegisters()
+	{
+		final NetworkInfo ni = ((ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE)).getActiveNetworkInfo();
+
+		if (ni == null) {
+			Log.e(LOGTAG, "no NetworkInfo found");
+			return;
+		}
+
+		Log.i(LOGTAG, "NetworkInfo " + ni.getTypeName() + " " + ni.getState());
+		if (ni.isConnected()) {
+			mLinphoneThread.refreshRegisters();
+		}
 	}
 
 	@Override
