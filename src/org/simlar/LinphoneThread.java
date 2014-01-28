d31c3978151ab9df91c9b438028fcbfb421abbad
Ben Sartor <mail@ben-sartor.de>
2014-01-28 15:55:49 +0100

    [LinphoneThread] set quality to unusable if up or download bandwidth is zero

--- src/org/simlar/LinphoneThread.java
+++ src/org/simlar/LinphoneThread.java
@@ -552,11 +552,13 @@ public class LinphoneThread
 
 			final int upload = Math.round(stats.getUploadBandwidth() / 8.0f * 10.0f); // upload bandwidth in 100 Bytes / second
 			final int download = Math.round(stats.getDownloadBandwidth() / 8.0f * 10.0f); // download bandwidth in 100 Bytes / second
-			final float quality = call.getCurrentQuality();
 			final PayloadType payloadType = call.getCurrentParamsCopy().getUsedAudioCodec();
 			final String codec = payloadType.getMime() + " " + payloadType.getRate() / 1000;
 			final String iceState = stats.getIceState().toString();
 			final int duration = call.getDuration();
+
+			// set quality to unusable if up or download bandwidth is zero
+			final float quality = (upload > 0 && download > 0) ? call.getCurrentQuality() : 0;
 
 			Log.i(LOGTAG, "callStatsUpdated: number=" + getNumber(call) + " upload=" + upload + " download=" + download + " quality=" + quality
 					+ " codec=" + codec + " iceState=" + iceState + " duration=" + duration);
