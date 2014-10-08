ec2fa6a898c7de19eed29d95b9821fa4bc3330f1
Ben Sartor <mail@ben-sartor.de>
2014-10-08 14:53:05 +0200

    [create-tag] now uses paramters for version numbers like 1.0.0

--- create-tag.sh
+++ create-tag.sh
@@ -3,27 +3,33 @@
 ## exit if an error occurs or on unset variables
 set -eu -o pipefail
 
+declare -r USAGE="Usage example: $0 1 0 0"
+
+declare -ri VERSION_MAJOR=${1?${USAGE}}
+declare -ri VERSION_MINOR=${2?${USAGE}}
+declare -ri VERSION_BUGFIX=${3?${USAGE}}
+
 declare -r BUILD_SCRIPT="$(dirname $(readlink -f $0))/build-for-upload.sh"
 declare -r UPDATE_MANIFEST_SCRIPT="$(dirname $(readlink -f $0))/update-android-manifest.sh"
 declare -r ANDROID_MANIFEST="$(dirname $(readlink -f $0))/AndroidManifest.xml"
 
-declare -r SIMLAR_VERSION_CODE="0.0.1 alpha $(date +'%Y-%m-%d')"
-declare -r SIMLAR_VERSION_TAG=$(echo "${SIMLAR_VERSION_CODE}" | sed "s/[[:space:]]/_/g")
+declare -r SIMLAR_VERSION="${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_BUGFIX}"
+echo "creating tag: '${SIMLAR_VERSION}'"
 
 git checkout master
 git fetch
 git fetch --tags
 git pull --rebase
 
-"${UPDATE_MANIFEST_SCRIPT}" "${SIMLAR_VERSION_CODE}" "${ANDROID_MANIFEST}"
+"${UPDATE_MANIFEST_SCRIPT}" "${SIMLAR_VERSION}" "${ANDROID_MANIFEST}"
 
 git add "${ANDROID_MANIFEST}"
-git commit -m "[Release] Version: ${SIMLAR_VERSION_CODE}"
+git commit -m "[Release] Version: ${SIMLAR_VERSION}"
 git push
 
-git tag -a "${SIMLAR_VERSION_TAG}" -m "Version: ${SIMLAR_VERSION_CODE}"
-git push origin "${SIMLAR_VERSION_TAG}"
-git checkout "${SIMLAR_VERSION_TAG}"
+git tag -a "${SIMLAR_VERSION}" -m "Version: ${SIMLAR_VERSION}"
+git push origin "${SIMLAR_VERSION}"
+git checkout "${SIMLAR_VERSION}"
 
 "${BUILD_SCRIPT}"
 
