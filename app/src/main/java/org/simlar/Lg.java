848d39392aef60f264eb16fc8e5b23ea684e399d
Ben Sartor <ben@simlar.org>
2015-03-26 04:17:30 +0100

    [Lg] Code Cleanup: removed suppress warning unused for Lg.v

--- app/src/main/java/org/simlar/Lg.java
+++ app/src/main/java/org/simlar/Lg.java
@@ -110,7 +110,6 @@ final class Lg
 		return mLevel < LOG_LEVEL_NORMAL;
 	}
 
-	@SuppressWarnings({ "unused" })
 	public static void v(final String tag, final Object... messageParts)
 	{
 		println(Log.VERBOSE, tag, null, messageParts);
