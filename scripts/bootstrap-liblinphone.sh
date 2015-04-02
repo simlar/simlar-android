bf264a5b3c488984d7b0baf20d23494ade610e8a
Ben Sartor <ben@simlar.org>
2015-04-02 11:33:03 +0200

    [bootstrap-liblinphone.sh] fixed applying patches

--- scripts/bootstrap-liblinphone.sh
+++ scripts/bootstrap-liblinphone.sh
@@ -6,12 +6,15 @@ set -eu -o pipefail
 declare -r BRANCH=${1:-""}
 
 declare -r COMPILE_SCRIPT="$(dirname $(readlink -f $0))/compile-liblinphone.sh"
-declare -r LINPHONE_ANDROID_PATCH_DIR="$(dirname $(readlink -f $0))/liblinphone/patches/linphone-android"
-declare -r LINPHONE_PATCH_DIR="$(dirname $(readlink -f $0))/liblinphone/patches/linphone"
-declare -r MEDIASTREAMER2_PATCH_DIR="$(dirname $(readlink -f $0))/liblinphone/patches/mediastreamer2"
-declare -r BELLESIP_PATCH_DIR="$(dirname $(readlink -f $0))/liblinphone/patches/belle-sip"
-declare -r ORTP_PATCH_DIR="$(dirname $(readlink -f $0))/liblinphone/patches/ortp"
-declare -r BZRTP_PATCH_DIR="$(dirname $(readlink -f $0))/liblinphone/patches/bzrtp"
+
+declare -r PATCH_DIR="$(dirname $(readlink -f $0))/../liblinphone/patches"
+
+declare -r LINPHONE_ANDROID_PATCH_DIR="${PATCH_DIR}/linphone-android"
+declare -r LINPHONE_PATCH_DIR="${PATCH_DIR}/linphone"
+declare -r MEDIASTREAMER2_PATCH_DIR="${PATCH_DIR}/mediastreamer2"
+declare -r BELLESIP_PATCH_DIR="${PATCH_DIR}/belle-sip"
+declare -r ORTP_PATCH_DIR="${PATCH_DIR}/ortp"
+declare -r BZRTP_PATCH_DIR="${PATCH_DIR}/bzrtp"
 
 declare -r BUILD_DIR="liblinphone/builds/$(date '+%Y%m%d_%H%M%S')"
 mkdir -p "${BUILD_DIR}"
