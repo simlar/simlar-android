05d45ae443f376d7cf541be18c7beb96d41dec68
Ben Sartor <ben@simlar.org>
2015-04-30 23:04:55 +0200

    [ProximityScreenLocker] Code Cleanup: removed unnecessary public

--- app/src/main/java/org/simlar/ProximityScreenLocker.java
+++ app/src/main/java/org/simlar/ProximityScreenLocker.java
@@ -2,9 +2,9 @@ package org.simlar;
 
 public interface ProximityScreenLocker
 {
-	public void acquire();
+	void acquire();
 
 	/// Not immediately means we wait till the proximity sensor signals that the user is not near anymore
 	@SuppressWarnings("SameParameterValue")
-	public void release(final boolean immediately);
+	void release(final boolean immediately);
 }
