085a595f81d6556a203ff9b547636aa47e47afd8
Ben Sartor <ben@simlar.org>
2015-06-03 18:25:51 +0200

    Revert "some animation shit"
    This reverts commit 699b18aa00ad8cb4fc0cf91e9ca871ad9c3105a3.

--- app/src/main/java/org/simlar/widgets/VideoFragment.java
+++ app/src/main/java/org/simlar/widgets/VideoFragment.java
@@ -102,8 +102,6 @@ public class VideoFragment extends Fragment
 		});
 
 		mCaptureView.setOnClickListener(view1 -> {
-			//view.animate().setDuration(2000).rotationYBy(180);
-			//mCaptureView.animate().setDuration(2000).scaleX(2).scaleY(2);
 			if (mListener != null) {
 				mListener.onCaptureViewClick();
 			}
@@ -160,10 +158,9 @@ public class VideoFragment extends Fragment
 
 	public final void setNowPlaying()
 	{
-//		final ViewGroup.LayoutParams params = mCaptureView.getLayoutParams();
-//		params.width = (int)(120 * getResources().getDisplayMetrics().density);
-//		params.height = (int)(145 * getResources().getDisplayMetrics().density);
-//		mCaptureView.setLayoutParams(params);
-		mCaptureView.animate().setDuration(2000).scaleXBy(2).scaleYBy(2);
+		final ViewGroup.LayoutParams params = mCaptureView.getLayoutParams();
+		params.width = (int)(120 * getResources().getDisplayMetrics().density);
+		params.height = (int)(145 * getResources().getDisplayMetrics().density);
+		mCaptureView.setLayoutParams(params);
 	}
 }
