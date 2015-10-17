5a349f377f7524423c48d331615c39803fa8b854
Ben Sartor <ben@simlar.org>
2015-10-17 17:12:39 +0200

    [create-tag.sh] now merges versionCode commit to master if needed

--- scripts/create-tag.sh
+++ scripts/create-tag.sh
@@ -31,8 +31,17 @@ git pull --rebase origin "${BRANCH}"
 "${UPDATE_MANIFEST_SCRIPT}" "${ANDROID_MANIFEST}"
 
 git add "${ANDROID_MANIFEST}"
-git commit -m "[Release] Version: ${SIMLAR_VERSION}"
+git commit -m "[AndroidManifest] increased versionCode for release ${SIMLAR_VERSION}"
 git push origin "${BRANCH}":"${BRANCH}"
+
+if [ "${BRANCH}" != "master" ] ; then
+	declare -r MANIFEST_COMMIT=$(git rev-parse --verify HEAD)
+	git checkout master
+	git pull --rebase
+	git cherry-pick "${MANIFEST_COMMIT}"
+	git push origin master
+	git checkout "${BRANCH}"
+fi
 
 git tag -a "${SIMLAR_VERSION}" -m "Version: ${SIMLAR_VERSION}"
 git push origin "${SIMLAR_VERSION}"
