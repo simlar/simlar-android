a2e4be1519a4319ff15c0ef077c7fdd00f37c12e
Ben Sartor <ben@simlar.org>
2016-04-09 17:30:11 +0200

    [RingingActivity] widgets are members now

--- app/src/main/java/org/simlar/widgets/RingingActivity.java
+++ app/src/main/java/org/simlar/widgets/RingingActivity.java
@@ -47,6 +47,9 @@ public final class RingingActivity extends AppCompatActivity
 	private final List<View> mCircles = new ArrayList<>();
 	private final SimlarServiceCommunicator mCommunicator = new SimlarServiceCommunicatorRinging();
 
+	private ImageView mContactImage;
+	private TextView mContactName;
+
 	private final class SimlarServiceCommunicatorRinging extends SimlarServiceCommunicator
 	{
 		@Override
@@ -82,6 +85,11 @@ public final class RingingActivity extends AppCompatActivity
 				WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
 				WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
 				WindowManager.LayoutParams.FLAG_IGNORE_CHEEK_PRESSES);
+
+
+
+		mContactImage = findViewById(R.id.contactImage);
+		mContactName = findViewById(R.id.contactName);
 
 		final Animation logoAnimation = AnimationUtils.loadAnimation(this, R.anim.rotate_logo);
 		final ImageView logo = findViewById(R.id.logo);
@@ -209,11 +217,8 @@ public final class RingingActivity extends AppCompatActivity
 			finish();
 		}
 
-		final ImageView contactImage = findViewById(R.id.contactImage);
-		final TextView contactName = findViewById(R.id.contactName);
-
-		contactImage.setImageBitmap(simlarCallState.getContactPhotoBitmap(this, R.drawable.contact_picture));
-		contactName.setText(simlarCallState.getContactName());
+		mContactImage.setImageBitmap(simlarCallState.getContactPhotoBitmap(this, R.drawable.contact_picture));
+		mContactName.setText(simlarCallState.getContactName());
 	}
 
 	@SuppressWarnings({ "unused", "RedundantSuppression" })
