eca0a06360fb5b87bedc78fd23228ceb5a99ba35
Ben Sartor <ben@simlar.org>
2017-03-15 03:39:30 +0100

    [ContactsAdapter] remove support for sdk version < 14

--- app/src/main/java/org/simlar/widgets/ContactsAdapter.java
+++ app/src/main/java/org/simlar/widgets/ContactsAdapter.java
@@ -21,7 +21,6 @@
 package org.simlar.widgets;
 
 import android.content.Context;
-import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -136,13 +135,7 @@ final class ContactsAdapter extends ArrayAdapter<ContactDataComplete>
 
 	public void addAllContacts(final Set<ContactDataComplete> contacts)
 	{
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-			super.addAll(contacts);
-		} else {
-			for (final ContactDataComplete contact : contacts) {
-				super.add(contact);
-			}
-		}
+		super.addAll(contacts);
 		sort(new SortByName());
 	}
 
