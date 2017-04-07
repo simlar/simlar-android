ae083b1ed38e1db9a32d813ba62f1c747d62fd8c
Ben Sartor <ben@simlar.org>
2017-04-07 23:09:30 +0200

    [resize_images.sh] remove support for sdk version 14

--- scripts/resize_images.sh
+++ scripts/resize_images.sh
@@ -28,18 +28,3 @@ find "${RES_DIR}"/drawable-xxxhdpi/ -type f -printf "%f\n" | sort | while read I
 	convert "${RES_DIR}"/drawable-xxxhdpi/"${IMAGE}" -resize 25%    "${RES_DIR}"/drawable-mdpi/"${IMAGE}"
 	convert "${RES_DIR}"/drawable-xxxhdpi/"${IMAGE}" -resize 18.75% "${RES_DIR}"/drawable-ldpi/"${IMAGE}"
 done
-
-rm -rf "${RES_DIR}"/drawable-xxhdpi-v11/*
-rm -rf "${RES_DIR}"/drawable-xhdpi-v11/*
-rm -rf "${RES_DIR}"/drawable-hdpi-v11/*
-rm -rf "${RES_DIR}"/drawable-mdpi-v11/*
-rm -rf "${RES_DIR}"/drawable-ldpi-v11/*
-
-find "${RES_DIR}"/drawable-xxxhdpi-v11/ -type f -printf "%f\n" | sort | while read IMAGE; do
-	git grep -q ${IMAGE%.*} || echo "WARNING: file not used: ${IMAGE}"
-	convert "${RES_DIR}"/drawable-xxxhdpi-v11/"${IMAGE}" -resize 75%    "${RES_DIR}"/drawable-xxhdpi-v11/"${IMAGE}"
-	convert "${RES_DIR}"/drawable-xxxhdpi-v11/"${IMAGE}" -resize 50%    "${RES_DIR}"/drawable-xhdpi-v11/"${IMAGE}"
-	convert "${RES_DIR}"/drawable-xxxhdpi-v11/"${IMAGE}" -resize 37.5%  "${RES_DIR}"/drawable-hdpi-v11/"${IMAGE}"
-	convert "${RES_DIR}"/drawable-xxxhdpi-v11/"${IMAGE}" -resize 25%    "${RES_DIR}"/drawable-mdpi-v11/"${IMAGE}"
-	convert "${RES_DIR}"/drawable-xxxhdpi-v11/"${IMAGE}" -resize 18.75% "${RES_DIR}"/drawable-ldpi-v11/"${IMAGE}"
-done
