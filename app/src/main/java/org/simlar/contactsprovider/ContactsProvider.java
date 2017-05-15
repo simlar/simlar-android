37399b8f30ee8975789e0a0b83e8c5e60beb5b76
Ben Sartor <ben@simlar.org>
2017-05-15 23:30:41 +0200

    [ContactsProvider] use ternary operator instead of if statement

--- app/src/main/java/org/simlar/contactsprovider/ContactsProvider.java
+++ app/src/main/java/org/simlar/contactsprovider/ContactsProvider.java
@@ -304,11 +304,9 @@ public final class ContactsProvider
 					continue;
 				}
 
-				String photoUri = null;
-				if (hasPhotoId) {
-					photoUri = Uri.withAppendedPath(ContentUris.withAppendedId(
-							ContactsContract.Contacts.CONTENT_URI, contactId), ContactsContract.Contacts.Photo.CONTENT_DIRECTORY).toString();
-				}
+				final String photoUri = hasPhotoId
+						? Uri.withAppendedPath(ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI, contactId), ContactsContract.Contacts.Photo.CONTENT_DIRECTORY).toString()
+						: null;
 
 				if (!result.containsKey(simlarId) || Util.isNullOrEmpty(result.get(simlarId).name)) {
 					result.put(simlarId, new ContactData(Util.equalString(name, number) ? "" : name,
