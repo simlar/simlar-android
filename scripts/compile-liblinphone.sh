636c762c07c802631d60408a091e7f84921e9a01
Ben Sartor <ben@simlar.org>
2017-10-22 14:10:58 +0200

    [compile-liblinphone.sh] remove old libs

--- scripts/compile-liblinphone.sh
+++ scripts/compile-liblinphone.sh
@@ -80,6 +80,7 @@ unzip -o $(find "${BUILD_DIR}/linphone-android/bin/distributions/" -maxdepth 1 -
 
 ## Android Studio
 rm -rf app/src/main/jniLibs/
+rm -f app/libs/*
 mv linphone-android-release.aar linphone-android-javadoc.jar linphone-android-sources.jar app/libs/
 
 echo "liblinphone build successfull with git hash: ${GIT_HASH}"
