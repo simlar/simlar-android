52da0428e85a89d32e4784b171236f2942c2cc3c
Ben Sartor <ben@simlar.org>
2020-08-28 16:36:51 +0200

    [Util] format

--- app/src/main/java/org/simlar/utils/Util.java
+++ app/src/main/java/org/simlar/utils/Util.java
@@ -132,7 +132,8 @@ public final class Util
 		return service;
 	}
 
-	public static <T> T defaultIfNull(final T value, final T defaultValue) {
+	public static <T> T defaultIfNull(final T value, final T defaultValue)
+	{
 		return value == null ? defaultValue : value;
 	}
 }
