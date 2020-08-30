02c9ce1ca562a6d46447d4c775528ba43ed7957d
Ben Sartor <ben@simlar.org>
2020-08-30 14:34:48 +0200

    [update-dockerfile.sh] update dockerfile to latest versions

--- scripts/update-dockerfile.sh
+++ scripts/update-dockerfile.sh
@@ -8,9 +8,21 @@ declare -r SDK_MANAGER=${SDK_MANAGER:-"${ANDROID_HOME}/cmdline-tools/latest/bin/
 declare -r GSED=$(which gsed)
 declare -r SED=${GSED:-"$(which sed)"}
 
+declare -r GREADLINK=$(which greadlink)
+declare -r READLINK=${GREADLINK:-"$(which readlink)"}
+
+declare -r PROJECT_DIR="$(dirname $(${READLINK} -f $0))/.."
+declare -r DOCKERFILE="${PROJECT_DIR}/docker-files/Dockerfile"
+
 function latestVersion() {
   local -r PACKAGE=$1
   "${SDK_MANAGER}" --list | "${SED}" -n "s/^[[:space:]]*${PACKAGE}\([^[:space:]]*\).*/\1/p" | sort --human-numeric-sort | uniq | tail -1
+}
+
+function updateDockerfile() {
+  local -r PACKAGE=$1
+  local -r VERSION=$2
+  "${SED}" -i "s/ENV ${PACKAGE} .*/ENV ${PACKAGE} \"${VERSION}\"/" "${DOCKERFILE}"
 }
 
 declare -r ANDROID_SDK_VERSION=$(latestVersion "platforms;android-")
@@ -21,3 +33,6 @@ echo "android sdk: ${ANDROID_SDK_VERSION}"
 echo "android build tools: ${ANDROID_BUILD_TOOLS_VERSION}"
 echo "android ndk: ${ANDROID_NDK_VERSION}"
 
+updateDockerfile "ANDROID_SDK_VERSION" "${ANDROID_SDK_VERSION}"
+updateDockerfile "ANDROID_BUILD_TOOLS_VERSION" "${ANDROID_BUILD_TOOLS_VERSION}"
+updateDockerfile "ANDROID_NDK_VERSION" "${ANDROID_NDK_VERSION}"
