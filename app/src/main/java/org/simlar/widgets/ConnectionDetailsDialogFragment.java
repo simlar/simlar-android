0db991faf77895146279752117f016f2ae7dd068
Ben Sartor <ben@simlar.org>
2020-12-04 19:45:34 +0100

    [ConnectionDetailsDialogFragment] use view binding

--- app/src/main/java/org/simlar/widgets/ConnectionDetailsDialogFragment.java
+++ app/src/main/java/org/simlar/widgets/ConnectionDetailsDialogFragment.java
@@ -21,11 +21,9 @@
 
 package org.simlar.widgets;
 
-import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
@@ -33,23 +31,14 @@ import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.FragmentActivity;
 
 import org.simlar.R;
+import org.simlar.databinding.DialogFragmentConnectionDetailsBinding;
 import org.simlar.helper.CallConnectionDetails;
 import org.simlar.logging.Lg;
 
 @SuppressWarnings("WeakerAccess") // class must be public otherwise crashes
 public final class ConnectionDetailsDialogFragment extends DialogFragment
 {
-	// gui elements
-	private TextView mTextViewQuality = null;
-	private TextView mTextViewUpload = null;
-	private TextView mTextViewDownload = null;
-	private TextView mTextViewIceState = null;
-	private TextView mTextViewCodec = null;
-	private TextView mTextViewJitter = null;
-	private TextView mTextViewPacketLoss = null;
-	private TextView mTextViewLatePackets = null;
-	private TextView mTextViewRoundTripDelay = null;
-	private TextView mTextViewEncryptionDescription = null;
+	private DialogFragmentConnectionDetailsBinding mBinding = null;
 
 	@NonNull
 	@Override
@@ -69,21 +58,15 @@ public final class ConnectionDetailsDialogFragment extends DialogFragment
 
 	private View createView(final FragmentActivity activity)
 	{
-		@SuppressLint("InflateParams")
-		final View view = activity.getLayoutInflater().inflate(R.layout.dialog_fragment_connection_details, null);
+		mBinding = DialogFragmentConnectionDetailsBinding.inflate(activity.getLayoutInflater());
+		return mBinding.getRoot();
+	}
 
-		mTextViewQuality = view.findViewById(R.id.textViewQuality);
-		mTextViewUpload = view.findViewById(R.id.textViewUpload);
-		mTextViewDownload = view.findViewById(R.id.textViewDownload);
-		mTextViewIceState = view.findViewById(R.id.textViewIceState);
-		mTextViewCodec = view.findViewById(R.id.textViewCodec);
-		mTextViewJitter = view.findViewById(R.id.textViewJitter);
-		mTextViewPacketLoss = view.findViewById(R.id.textViewPacketLoss);
-		mTextViewLatePackets = view.findViewById(R.id.textViewLatePackets);
-		mTextViewRoundTripDelay = view.findViewById(R.id.textViewRoundTripDelay);
-		mTextViewEncryptionDescription = view.findViewById(R.id.textViewEncryptionDescription);
-
-		return view;
+	@Override
+	public void onDestroyView()
+	{
+		mBinding = null;
+		super.onDestroyView();
 	}
 
 	public void setCallConnectionDetails(final CallConnectionDetails callConnectionDetails)
@@ -96,15 +79,15 @@ public final class ConnectionDetailsDialogFragment extends DialogFragment
 			return;
 		}
 
-		mTextViewQuality.setText(getString(callConnectionDetails.getQualityDescription()));
-		mTextViewUpload.setText(getString(R.string.connection_details_dialog_fragment_kilobytes_per_second, callConnectionDetails.getUpload()));
-		mTextViewDownload.setText(getString(R.string.connection_details_dialog_fragment_kilobytes_per_second, callConnectionDetails.getDownload()));
-		mTextViewIceState.setText(callConnectionDetails.getIceState());
-		mTextViewCodec.setText(callConnectionDetails.getCodec());
-		mTextViewJitter.setText(callConnectionDetails.getJitter());
-		mTextViewPacketLoss.setText(getString(R.string.connection_details_dialog_fragment_percent, callConnectionDetails.getPacketLoss()));
-		mTextViewLatePackets.setText(callConnectionDetails.getLatePackets());
-		mTextViewRoundTripDelay.setText(getString(R.string.connection_details_dialog_fragment_milli_seconds, callConnectionDetails.getRoundTripDelay()));
-		mTextViewEncryptionDescription.setText(callConnectionDetails.getEncryptionDescription());
+		mBinding.textViewQuality.setText(getString(callConnectionDetails.getQualityDescription()));
+		mBinding.textViewUpload.setText(getString(R.string.connection_details_dialog_fragment_kilobytes_per_second, callConnectionDetails.getUpload()));
+		mBinding.textViewDownload.setText(getString(R.string.connection_details_dialog_fragment_kilobytes_per_second, callConnectionDetails.getDownload()));
+		mBinding.textViewIceState.setText(callConnectionDetails.getIceState());
+		mBinding.textViewCodec.setText(callConnectionDetails.getCodec());
+		mBinding.textViewJitter.setText(callConnectionDetails.getJitter());
+		mBinding.textViewPacketLoss.setText(getString(R.string.connection_details_dialog_fragment_percent, callConnectionDetails.getPacketLoss()));
+		mBinding.textViewLatePackets.setText(callConnectionDetails.getLatePackets());
+		mBinding.textViewRoundTripDelay.setText(getString(R.string.connection_details_dialog_fragment_milli_seconds, callConnectionDetails.getRoundTripDelay()));
+		mBinding.textViewEncryptionDescription.setText(callConnectionDetails.getEncryptionDescription());
 	}
 }
