4bdbc208b45da728e44321cc62b4d92aba541df0
Ben Sartor <ben@simlar.org>
2020-12-21 22:35:42 +0100

    WIP [update-linphone-sdk.sh] script to create linphone update commits

---
+++ scripts/update-linphone-sdk.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+## exit if an error occurs or on unset variables
+set -eu -o pipefail
+
+declare -r USAGE="Usage example: $0 \"4.4.15\" \"4.4.20\""
+
+declare -r OLD_VERSION=${1?${USAGE}}
+declare -r NEW_VERSION=${2?${USAGE}}
+
+
+git-create-space-commits.sh "${NEW_VERSION}"
+
+
+sed -i "s/liblinphoneVersion = \"${OLD_VERSION}\"/liblinphoneVersion = \"${NEW_VERSION}\"/" app/build.gradle
+git commit -am "[gradle] update ${OLD_VERSION} -> ${NEW_VERSION}"
+
+
+find app/src/push/play/release-notes/ -name alpha.txt -exec sed -i "s/${OLD_VERSION}/${NEW_VERSION}/" {} \;
+git commit -am "[play] update release notes liblinphone ${NEW_VERSION}"
+
+
+sed -i "s/${OLD_VERSION}/${NEW_VERSION}/" scripts/bootstrap-liblinphone.sh
+git commit -am "[bootstrap-liblinphone.sh] default to version ${NEW_VERSION}"
+
+
+time docker run --cap-drop all --security-opt=no-new-privileges -it --rm -v $(pwd)-docker-gradle-cache:/home/builder/.gradle -v $(pwd):/pwd -e CMAKE_BUILD_PARALLEL_LEVEL=16 simlar-android-builder:latest bash -c "cd /pwd && ./scripts/bootstrap-liblinphone.sh"
+git add app/libs/
+git commit -m "[liblinphone] rebuild version ${NEW_VERSION}"
+git revert HEAD --no-edit
