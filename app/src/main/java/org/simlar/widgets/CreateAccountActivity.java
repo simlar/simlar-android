c0067f1d8f0604893a484e0a93d9fb7506093ebe
Ben Sartor <ben@simlar.org>
2025-07-04 11:30:43 +0200

    Revert "[CreateAccountActivity] fix edge to edge layout introduced with Android SDK 35"
    This reverts commit 59115db0e3451a9ded1da8122d47c70857cdb0ca.

--- app/src/main/java/org/simlar/widgets/CreateAccountActivity.java
+++ app/src/main/java/org/simlar/widgets/CreateAccountActivity.java
@@ -142,8 +142,6 @@ public final class CreateAccountActivity extends AppCompatActivity
 		Lg.i("onCreate");
 
 		setContentView(R.layout.activity_create_account);
-		Util.edge2edgeLayout(findViewById(R.id.layoutCreateAccountActivity));
-		Lg.i("** WTF");
 
 		setFinishOnTouchOutside(false);
 
